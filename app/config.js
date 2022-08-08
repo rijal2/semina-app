9140c092ded17b480a110f37345ffda4f4d459ac
Muchamad Rijal <merentas5benua27@gmail.com>
2022-08-09 01:04:05 +0700

    Menyiapkan dan melakukan configurasi token di folder utils

--- app/config.js
+++ app/config.js
@@ -1,6 +1,8 @@
-const dotenv = require('dotenv');
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
 module.exports = {
-    urlDb: process.env.URL_MONGODB_DEV,
-}
+  urlDb: process.env.URL_MONGODB_DEV,
+  jwtExpiration: "24h",
+  jwtSecret: "jwtSecret",
+};
